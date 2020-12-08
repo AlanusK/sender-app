7373577a00b7e010f6663c1a4571c62cb8e6297b
Alan <akajara@gmail.com>
2020-12-08 14:51:27 +0300

    📦 NEW: Add Sender Container

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WalletBallanceContainer from "../../containers/WalletBallanceContainer/WalletBallanceContainer";
+import SenderContainer from "../../containers/SenderContainer/SenderContainer";
 
 const Dashboard = () => {
   return (
     <div className="site-wrapper">
-      <WalletBallanceContainer />
+      {/* <WalletBallanceContainer /> */}
+      <SenderContainer />
     </div>
   );
 };
