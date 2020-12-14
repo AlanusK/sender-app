a357d3830020f02f7953a98d492d9d6701c4420f
Alan <akajara@gmail.com>
2020-12-14 12:04:20 +0300

    🐞 FIX: Fix exporting errors

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -3,5 +3,22 @@ import ExtendedWalletBallanceContainer from "./ExtendedWalletBallanceContainer/E
 import SelectCurrencyContainer from "./SelectCurrencyContainer/SelectCurrencyContainer";
 import WalletBallanceContainer from "./WalletBallanceContainer/WalletBallanceContainer";
 import KycContainer from "./KycContainer/KycContainer";
+import TransactionsTableContainer from "./TransactionsTableContainer/TransactionsTableContainer";
+import WithdrawalFormContainer from "./WithdrawalFormContainer/WithdrawalFormContainer";
+import SendMoneyContainer from "./SendMoneyContainer/SendMoneyContainer";
+import GeneralSettingsContainer from "./GeneralSettingsContainer/GeneralSettingsContainer";
+import SettingsContainer from "./SettingsSidebarMenuContainer/SettingsContainer";
 
-export {WalletBallanceContainer, ExtendedWalletBallanceContainer, KycContainer }
+export {
+  WalletBallanceContainer,
+  ExtendedWalletBallanceContainer,
+  KycContainer,
+  SelectCurrencyContainer,
+  DepositFormContainer,
+  WithdrawalFormContainer,
+  TransactionsTableContainer,
+  SendMoneyContainer,
+  SettingsContainer,
+  GeneralSettingsContainer
+
+};
