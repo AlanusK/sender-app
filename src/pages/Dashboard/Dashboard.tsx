09c53654e3076b51a3e6cc35c576abfbed3eac6b
Omar <ommyjay@gmail.com>
2020-12-15 18:26:38 +0300

    👌 IMPROVE: update dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -80,24 +80,36 @@ const columns: ColumnsType<transactions> = [
 const Dashboard = () => {
   return (
     <div className="site-wrapper">
-      <KycContainer/>
-      <TransactionsTableContainer columns={columns} transactions={data} />
       <div style={{ marginTop: "40px" }}>
+        <h1>Extensive KYC Form</h1>
+        <KycContainer />
+      </div>
+      <div style={{ marginTop: "40px" }}>
+        <h1>Transaction Table</h1>
+        <TransactionsTableContainer columns={columns} transactions={data} />
+      </div>
+      <div style={{ marginTop: "40px" }}>
+        <h1>Wallet Balance Cards</h1>
         <WalletBallanceContainer />
       </div>
       <div style={{ marginTop: "40px" }}>
+        <h1> Extended Wallet Balance Cards </h1>
         <ExtendedWalletBallanceContainer />
       </div>
       <div style={{ marginTop: "40px" }}>
+        <h1>Select Currency Components</h1>
         <SelectCurrencyContainer />
       </div>
       <div style={{ marginTop: "80px" }}>
+        <h1>Deposit Form Container</h1>
         <DepositFormContainer />
       </div>
       <div style={{ marginTop: "20px" }}>
+        <h1>Withdrawal Form Container</h1>
         <WithdrawalFormContainer />
       </div>
       <div style={{ marginTop: "80px" }}>
+        <h1>Send Money Form Container</h1>
         <SendMoneyContainer />
       </div>
     </div>
