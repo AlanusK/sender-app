c1a62b8678861b652206caa9bfcbf2eda95c81af
Alan <akajara@gmail.com>
2021-01-19 12:11:03 +0300

    👌 IMPROVE: Add transaction table to dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,89 @@
 import React from "react";
+import {
+  TransactionsTableContainer,
+  SendMoneyContainer,
+} from "../../containers";
+import { ColumnsType } from "antd/lib/table";
+import "./Dashboard.css";
+import { Row, Col } from "antd";
+
+const data = [
+  {
+    key: "1",
+    date: "04/11/1990",
+    amount: "USD 10,000/=",
+    type: "Deposit",
+    status: "Completed",
+  },
+  {
+    key: "2",
+    date: "28/05/2020",
+    amount: "USD 1,000/=",
+    type: "Deposit",
+    status: "Completed",
+  },
+  {
+    key: "3",
+    date: "17/11/2014",
+    amount: "EUR 1,000/=",
+    type: "Deposit",
+    status: "Completed",
+  },
+  {
+    key: "4",
+    date: "13/09/2007",
+    amount: "EUR 1,000/=",
+    type: "Deposit",
+    status: "Cancelled",
+  },
+];
+
+type transactions = {
+  key: string;
+  date: string;
+  amount: string;
+  type: string;
+  status: string;
+};
+
+const columns: ColumnsType<transactions> = [
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+    // align: 'center',
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    key: "amount",
+    align: "right",
+  },
+  {
+    title: "Type",
+    dataIndex: "type",
+    key: "type",
+    align: "left",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    key: "status",
+    // align: 'center',
+  },
+];
 
 
 const Dashboard = () => {
   return (
-    <div className="site-wrapper">
-      <h1> Dashboard</h1>
-    </div>
+    <Row className="site-wrapper">
+      <Col className="transaction-table-column" flex="auto"> 
+        <TransactionsTableContainer columns={columns} transactions={data} />
+      </Col>
+      {/* <Col className="send-money-column" flex="420px" >
+        <SendMoneyContainer />  
+      </Col> */}
+    </Row>
   );
 };
 
