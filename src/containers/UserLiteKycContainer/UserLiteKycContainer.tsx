0ffc57c6c6aa9dfcd49c824c66a1934d1df8c4a5
Alan <akajara@gmail.com>
2021-01-19 14:06:18 +0300

    👌 IMPROVE: Code structure

--- src/containers/UserLiteKycContainer/UserLiteKycContainer.tsx
+++ src/containers/UserLiteKycContainer/UserLiteKycContainer.tsx
@@ -21,83 +21,80 @@ const UserLiteKycContainer = () => {
     // };
 
     return (
-        <>
-            <div className="user-lite-KYC-container-wrapper">
-                <div className="user-lite-form-wrapper">
-                    <Form
-                        name="normal_login"
-                        className="login-form"
-                        initialValues={{ remember: true }}
-                    // onFinish={onFinish}
+        <div className="user-lite-KYC-container-wrapper">
+            <div className="user-lite-form-wrapper">
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                // onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="firstName"
+                        rules={[{ required: true, message: 'Please input your first name!' }]}
                     >
-                        <Form.Item
-                            name="firstName"
-                            rules={[{ required: true, message: 'Please input your first name!' }]}
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="First name" />
+                    </Form.Item>
+                    <Form.Item
+                        name="lastName"
+                        rules={[{ required: true, message: 'Please input your last name!' }]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Last name" />
+                    </Form.Item>
+                    <Form.Item
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email' }]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="repeatPassword"
+                        rules={[{ required: true, message: 'Please repeat your password!' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Repeat Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Checkbox
+                            checked={showWallet}
+                            onChange={onCheckboxChange}
                         >
-                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="First name" />
-                        </Form.Item>
-                        <Form.Item
-                            name="lastName"
-                            rules={[{ required: true, message: 'Please input your last name!' }]}
-                        >
-                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Last name" />
-                        </Form.Item>
-                        <Form.Item
-                            name="email"
-                            rules={[{ required: true, message: 'Please input your email' }]}
-                        >
-                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your Password!' }]}
-                        >
-                            <Input
-                                prefix={<LockOutlined className="site-form-item-icon" />}
-                                type="password"
-                                placeholder="Password"
-                            />
-                        </Form.Item>
-                        <Form.Item
-                            name="repeatPassword"
-                            rules={[{ required: true, message: 'Please repeat your password!' }]}
-                        >
-                            <Input
-                                prefix={<LockOutlined className="site-form-item-icon" />}
-                                type="password"
-                                placeholder="Repeat Password"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Checkbox
-                                checked={showWallet}
-                                onChange={onCheckboxChange}
-                            >
-                                Using existing Stellar wallet
+                            Using existing Stellar wallet
                             </Checkbox>
-                        </Form.Item>
-                        <Form.Item
-                            name="walletSecretKey"
-                            rules={[{ required: true, message: 'Please enter wallet secret key!' }]}
-                            hidden={!showWallet}
-                        >
-                            <Input
-                                prefix={<LockOutlined className="site-form-item-icon" />}
-                                type="password"
-                                placeholder="Wallet Secret Key"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Register
+                    </Form.Item>
+                    <Form.Item
+                        name="walletSecretKey"
+                        rules={[{ required: true, message: 'Please enter wallet secret key!' }]}
+                        hidden={!showWallet}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Wallet Secret Key"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Register
                             </Button>
                             Already have an account? <a href="/login">Login now!</a>
-                        </Form.Item>
-                    </Form>
-                </div>
+                    </Form.Item>
+                </Form>
             </div>
-
-        </>
+        </div>
     );
 };
 
