e65caa3386fa762ff88b83aa05c29f29af0488a5
Alan <akajara@gmail.com>
2021-01-20 18:42:33 +0300

    👌 IMPROVE: Add all transactions table container on transactions page

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,8 +1,80 @@
 import React from "react";
+import { AllTransactionsTableContainer } from "../../containers";
+import { ColumnsType } from "antd/lib/table";
+
+const data = [
+  {
+    key: "1",
+    date: "04/11/1990",
+    amount: "USD 10,000/=",
+    type: "Deposit",
+    status: "Completed",
+  },
+  {
+    key: "2",
+    date: "28/05/2020",
+    amount: "USD 1,000/=",
+    type: "Deposit",
+    status: "Completed",
+  },
+  {
+    key: "3",
+    date: "17/11/2014",
+    amount: "EUR 1,000/=",
+    type: "Deposit",
+    status: "Completed",
+  },
+  {
+    key: "4",
+    date: "13/09/2007",
+    amount: "EUR 1,000/=",
+    type: "Deposit",
+    status: "Cancelled",
+  },
+];
+
+type transactions = {
+  key: string;
+  date: string;
+  amount: string;
+  type: string;
+  status: string;
+};
+
+const columns: ColumnsType<transactions> = [
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+    // align: 'center',
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    key: "amount",
+    align: "right",
+  },
+  {
+    title: "Type",
+    dataIndex: "type",
+    key: "type",
+    align: "left",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    key: "status",
+    // align: 'center',
+  },
+];
 
 const Transactions = () => {
 
-  return <h1>Transactions</h1>;
+  return (
+    <div>
+      <AllTransactionsTableContainer columns={columns} transactions={data} />
+    </div>
+  );
 };
 
 export default Transactions;
