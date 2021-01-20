40092b894a502fc20c943859ccaf608bd5ae3ba6
Alan <akajara@gmail.com>
2021-01-20 13:33:00 +0300

    👌 IMPROVE: Add extended wallet ballance cards on wallet page

--- src/pages/Wallet/Wallet.tsx
+++ src/pages/Wallet/Wallet.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { ExtendedWalletBallanceContainer } from "../../containers";
+import "./Wallet.css";
 
 const Wallet = () => {
 
-  return <h1>Wallet</h1>;
+  return (
+    <>
+      <h1> Wallet </h1>
+      <hr className="wallet-line" />
+      <ExtendedWalletBallanceContainer />
+    </>
+  );
 };
 
 export default Wallet;
